7a0098563c1cf3a257dfefac1076465bb5ddb161
Thiago <contact.thiago.ferreira@gmail.com>
2023-10-30 15:38:08 +0100

    fix bug with deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,29 @@
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
-      <body>{children}</body>
+
+      <body className={`${inter.className} antialiased`}>
+
+        <header>
+          <nav>
+            <ul>
+              <li>Dashboard</li>
+              <li>Login</li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
+        </header>
+       
+        {children}</body>
     </html>
   );
 }
